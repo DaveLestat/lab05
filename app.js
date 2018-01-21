323e86b01f0c36363574d426316bfe6e49aecaff
DAVID CERINO <DAVID.CERINO.U@GMAIL.COM>
2018-01-20 18:32:08 -0800

    Finally, got number 4

--- app.js
+++ app.js
@@ -77,12 +77,22 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(testArray) {
+  var sum1 = 0;
+  for (var i = 0; i<testArray.length; i++){
+    sum1 = sum(testArray[i], sum1)[0];
+    console.log(i);}
+  var message = testArray + ' was passed in as an array of numbers, and '+sum1+' is their sum.';
+  var array4 = [sum1, message];
+  console.log(array4);
+  return array4;
+
+
   //eslint-disable-line
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
